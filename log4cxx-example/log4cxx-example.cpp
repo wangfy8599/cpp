6abe5f0505c97c0517b67559d197c0110b225887
fuyan.wang <fuyan.wang@thomsonreuters.com>
2019-03-28 22:21:40 +0800

    update

--- log4cxx-example/log4cxx-example.cpp
+++ log4cxx-example/log4cxx-example.cpp
@@ -2,10 +2,19 @@
 //
 
 #include "stdafx.h"
+#include <stdlib.h>
 
+#include <log4cxx/logger.h>
+#include <log4cxx/propertyconfigurator.h>
+
+using namespace log4cxx;
+using namespace log4cxx::helpers;
 
 int main(int argc, char* argv[])
 {
+	PropertyConfigurator::configure("./log4cxx.properties");
+	LoggerPtr logger = Logger::getLogger("");
+	LOG4CXX_INFO(logger, "This is a test");
 	return 0;
 }
 
