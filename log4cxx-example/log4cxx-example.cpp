94191d9435a2388b61f1166ba4c72f53df1f04ec
fuyan.wang <fuyan.wang@thomsonreuters.com>
2019-03-30 11:35:40 +0800

    update

--- log4cxx-example/log4cxx-example.cpp
+++ log4cxx-example/log4cxx-example.cpp
@@ -4,17 +4,30 @@
 #include "stdafx.h"
 #include <stdlib.h>
 
+#include <string>
+
 #include <log4cxx/logger.h>
 #include <log4cxx/propertyconfigurator.h>
 
 using namespace log4cxx;
 using namespace log4cxx::helpers;
 
+using namespace std;
+
 int main(int argc, char* argv[])
 {
 	PropertyConfigurator::configure("./log4cxx.properties");
 	LoggerPtr logger = Logger::getLogger("");
-	LOG4CXX_INFO(logger, "This is a test");
+	string msg("ÏûÏ¢");
+	for(int i=0; i<10; ++i)
+	{
+		LOG4CXX_TRACE(logger, "TRACE" << msg.c_str() << i << ".");
+		LOG4CXX_DEBUG(logger, "DEBUG" << msg.c_str() << i << ".");
+		LOG4CXX_INFO(logger, "INFO" << msg.c_str() << i << ".");
+		LOG4CXX_WARN(logger, "WARN" << msg.c_str() << i << ".");
+		LOG4CXX_ERROR(logger, "ERROR" << msg.c_str() << i << ".");
+		LOG4CXX_FATAL(logger, "FATAL" << msg.c_str() << i << ".");
+	}
 	return 0;
 }
 
